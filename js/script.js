14c1892ecef9c53896f580eab90f63dfca27162a
Jihad Noureddine <jihad.nd@gmail.com>
2020-10-05 10:04:36 +0300

    add slider to the book section

--- js/script.js
+++ js/script.js
@@ -47,8 +47,34 @@ $(window).scroll(function(e) {
     winScrollTop = $(this).scrollTop();
     parallax();
 });
- */
 $(window).scroll(function(e) {
 
     alert($(window).offset().top);
+});
+ */
+
+
+$(document).ready(function() {
+
+
+    var mySwiper = new Swiper('.swiper-container', {
+        // Optional parameters
+        loop: true,
+
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        // And if we need scrollbar
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+    })
 });
